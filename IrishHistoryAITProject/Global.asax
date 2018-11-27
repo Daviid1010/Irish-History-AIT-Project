668c1d91c3ac47937c985d30ce11778b3c04814a
David Sheehy <sheehy_d@ymail.com>
2018-11-27 10:38:03 +0000

    Add project files.

---
+++ IrishHistoryAITProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IrishHistoryAITProject.MvcApplication" Language="C#" %>
---
+++ IrishHistoryAITProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IrishHistoryAITProject.Startup))]
+namespace IrishHistoryAITProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
