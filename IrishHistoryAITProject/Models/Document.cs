f00230c7930708ac166673beaa7620bf15857bcc
David Sheehy <sheehy_d@ymail.com>
2018-12-02 18:10:33 +0000

    added document class which will represent important information regarding treaties etc.

--- IrishHistoryAITProject/Models/Document.cs
+++ IrishHistoryAITProject/Models/Document.cs
@@ -1,12 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 
 namespace IrishHistoryAITProject.Models
 {
-    public class Event
+    public class Document
     {
-        public int EventID { get; set; }
+        public int DocumentID { get; set; }
         public string Name { get; set; }
-        public string Date { get; set; }
+        public string Description { get; set; }
 
         public virtual Location Location { get; set; }
         public virtual ICollection<Person> People { get; set; }
